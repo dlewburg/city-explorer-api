703931cb4ceb153215846900bd980b2b36960d64
Dasha Burgos <dasha.lewis@gmail.com>
2023-03-24 21:31:58 -0400

    able to get cache weather data

--- modules/getMovies.js
+++ modules/getMovies.js
@@ -2,17 +2,45 @@
 
 const axios = require('axios');
 
+let cache = {};
+
+// Cache TODO : create a key for data to store
+// Cache TODO: if the thing exists and within a valid timefram... send the data from cache
+// Cache TODO: if it does not exist - call API and cache that return from the API
+
 async function getMovies(request, response, next) {
 
   try {
+
+    
     let { movieName } = request.query;
     let { cityName } = request.query;
-    let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${cityName}`
+    
+    let key = `${cityName}-Movie`; 
+    
+    if(cache[key] && (Date.now() - cache[key].timestamp) < 60000) {
+      console.log('Cache was hit!!!!!');
 
-    let movieResults = await axios.get(url);
-    let movieInfo = movieResults.data.results.map(movie => new Movie(movie));
+      response.status(200).send(cache[key].data);
 
-    response.status(200).send(movieInfo);
+      
+    } else {
+
+      console.log('Nothing in Cache');
+      
+      let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${cityName}`
+      let movieResults = await axios.get(url);
+      let movieInfo = movieResults.data.results.map(movie => new Movie(movie));
+      
+      // BUILD IT INTO CACHE
+      
+      cache[key] = {
+        data: movieInfo,
+        timestamp: Date.now()
+      };
+      
+      response.status(200).send(movieInfo);
+    }
 
   } catch (error) {
     next(error)
